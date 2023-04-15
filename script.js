20c6f731daabac432f23d902a2b5043f0f1c80c6
Virende9 <r.virenderverma1@gmail.com>
2023-04-15 14:24:03 +0530

    Changed the icon to defaulot need to work on it to change dynamically

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
 const apiKey = "e7e55771f9b815c9b8fa9f38416b55a4"; // openwether
-// const apiKey = "bac594f927a1693759b6a232708cb672"; //weather stack api
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric"; //open Weather api
-// const apiUrl = "http://api.weatherstack.com/current?&units=m";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric"; //open Weather api
 
 const searchVal = document.querySelector(".search input");
-const weatherIcon = document.querySelector(".weather_icon").value;
+let weatherIcon = document.getElementById(changeIcon);
 const btn = document.querySelector(".search button");
 
 async function weather(city) {
-  const response = await fetch(apiUrl + `&q=${city}` + `&appid=${apiKey}`); // open weather api rule
-  //   const response = await fetch(
-  //     apiUrl + `&query=${city}` + `&access_key=${apiKey}`
-  //   );
-  var data = response.json();
-  //   var current = response.json();
-  //   console.log(current);
+  let response = await fetch(apiUrl + `&q=${city}` + `&appid=${apiKey}`, {
+    origin: "cros",
+  }); // open weather api rule
+  var data = await response.json();
+  // console.log(data);
+  console.log(data.weather[0].main);
+
   // change if city name is valid or if invalid present the error
   if (response.status == 404) {
     //if incorrect data then change the weather to display the error and hide the weather details
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   } else {
-    //openweather api data
+    // openweather api data
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
-      Math.round(data.main.temp) + "<sup> &#8451</sup>";
+      data.main["temp"] + "<sup> &#8451</sup>";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-
-    //weatherstack api data fetch
-    // document.querySelector(".city").innerHTML = current.location.name;
-    // document.querySelector(".temp").innerHTML =
-    //   Math.round(current.temperature) + "<sup> &#8451</sup>";
-    // document.querySelector(".humidity").innerHTML = current.humidity + "%";
-    // document.querySelector(".wind").innerHTML = current.wind_speed + "km/h";
+    // weatherIcon.src = data.weather[0].main;
 
     // to change the images according to the data
-    if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "drizzle") {
-      weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main == "clear") {
-      weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "mist") {
-      weatherIcon.src = "images/mist.png";
-    } else if (data.weather[0].main == "snow") {
-      weatherIcon.src = "images/snow.png";
-    } else if (data.weather[0].main == "rain") {
-      weatherIcon.src = "images/rain.png";
-    }
+
+    // const icon = data.weather[0].main;
+    // if (icon == "Haze") {
+    //   weatherIcon.src == "images/snow.png";
+    // } else if (icon == "Clear") {
+    //   weatherIcon.src = "images/clear.png";
+    // } else if (icon == ["Clouds"]) {
+    //   weatherIcon.src = "images/drizzle.png";
+    //   console.log(weatherIcon.src);
+    // } else if (icon == "Mist") {
+    //   weatherIcon.src = ".images/mist.png";
+    // } else if (icon == "Snow") {
+    //   weatherIcon.src = "images/snow.png";
+    // } else if (icon == "Rain") {
+    //   weatherIcon.src = "images/rain.png";
+    // }
 
     //if correct data then change the weather to display the details and hide the error text
     document.querySelector(".weather").style.display = "block";
@@ -56,6 +51,7 @@ async function weather(city) {
   }
 }
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
   weather(searchVal.value);
 });
